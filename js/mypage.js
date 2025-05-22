6bdafc06cbd7a2de70366a943f9429f62034563e
Shalom-227 <alaladeshalom02@gmail.com>
2025-05-22 12:48:42 +0100

    printed log messages to console using console.log statements and created files mypage.css mypage.html & mypage.js

---
+++ js/mypage.js
@@ -0,0 +1,4 @@
+console.log("Hello, World"); /* prints a string to the console */
+console.log(typeof "Shalom"); /* prints the datatype string */
+console.log(typeof 227);     /*prints the datatype number */
+console.log(typeof false);  /* prints the datatype boolean */
