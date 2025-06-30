ea033b7d97e0e5803e2349c6a32af4a5a8b7aa5e
Shalom-227 <alaladeshalom02@gmail.com>
2025-06-30 15:52:28 +0100

    a program that sums the integers of an array

---
+++ js/loops/for_loop.js
@@ -0,0 +1,10 @@
+//a program that sums up integers in an array
+
+
+let values = [10, 30, 50, 100];
+let sum = 0;
+
+for (index = 0; index < values.length; index++){
+    sum += values[index];
+}
+console.log(sum);
